b21bbca6e665940a6321c1303952d0427481a16f
melaniecast <mllaniec89@gmail.com>
2026-04-16 11:11:19 -0600

    actualizacion clases pokemon

--- pokemon_clases.py
+++ pokemon_clases.py
@@ -1,7 +1,28 @@
 from abc import ABC, abstractmethod
 
-class Pokemon:
-    def __init__(self, nombre, __vida_maxima, __energia_maxima):
+class Pokemon(ABC):
+    def __init__(self, nombre, vida_maxima, energia_maxima):
         self.nombre = nombre
-        self.__vida_maxima = vida_maxima
+        self._vida_maxima = vida_maxima
+        self._energia_maxima = energia_maxima
+        self._vida_actua = vida_maxima
+        
+    
+    @property
+    def vida_actual(self):
+        return self._vida_actual
+        
+        
+    @abstractmethod
+    def atacar(self):
+        pass
+    @abstractmethod   
+    def defensa(self):
+        pass
+    @abstractmethod
+    def descanso(self):
+        pass    
+    
+    
+    
         
---
+++ pokemon_fuego.py
@@ -0,0 +1,14 @@
+from pokemon_clases import Pokemon
+
+class PokemonFuego(Pokemon):
+    def __init__(self, nombre, vida_maxima, energia_maxima):
+        super().__init__(nombre, vida_maxima, energia_maxima)
+        
+    def atacar(self):
+        return super().atacar()
+    
+    def defensa(self):
+        return super().defensa()
+    
+    def descanso(self):
+        return super().descanso()
