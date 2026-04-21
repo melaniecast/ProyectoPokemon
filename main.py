d39f5a85989368890ac2c74db5ea3c1ac59e108f
melaniecast <mllaniec89@gmail.com>
2026-04-21 16:46:38 -0600

    actualizacion opcion -menu

--- main.py
+++ main.py
@@ -8,89 +8,92 @@ from pokemon_fuego import PokemonFuego
 from pokemon_planta import PokemonPlanta
 # 
 
+
+    
+    
+def crear_pokemon(datos_jugador): 
+    tipo = datos_jugador['tipo']   
+        
+    if tipo == 'Agua':
+        return PokemonAgua(datos_jugador['nombre'], datos_jugador['hp_maximo'], datos_jugador['energia_maxima'])
+    elif tipo == 'Fuego':
+        return PokemonFuego(datos_jugador['nombre'], datos_jugador['hp_maximo'], datos_jugador['energia_maxima'])
+    elif tipo == 'Planta':
+        return PokemonPlanta(datos_jugador['nombre'], datos_jugador['hp_maximo'], datos_jugador['energia_maxima'])
+    elif tipo == 'Electrico':
+        return PokemonElectrico(datos_jugador['nombre'], datos_jugador['hp_maximo'], datos_jugador['energia_maxima'])
+    
+           
 def menu_batalla():
     print ('='*50)
     print('     SIMLADOR DE BATALLAS POKÉMON (POO)     ')
     print ('='*50)
     print('Selecciones el Modo de Juego: \n1. Jugador vs Jugador\n2. Jugador vs Computadora')
-    opcion = input('> Opción: ')
+    opcion = input('> Opción: ')     
+            
     
     if opcion =='1':
         mostrar_catalogo_disponible()
         
         eleccion_jugador_1 = input('Jugador 1, elija el numero de su Pokémon: ')
-        print(f'¡Has seleccionado a {eleccion_jugador_1}')
         eleccion_jugador_2 = input('Jugador 2, elija el numero de su Pokémon: ')
-        print(f'¡Has seleccionado a {eleccion_jugador_2}')
-        print('¡COMIENZA LA BATALLA!')
-        print(f'{eleccion_jugador_1} ({tipo}) vs {eleccion_jugador_2} ({tipo})')
         
         datos_jugador1 = CATALOGO_POKEMON[eleccion_jugador_1]
-        
-       
-        
-        
-        if tipo == 'Agua':
-            pokemon_jugador1 = PokemonAgua(datos_jugador1['nombre'], datos_jugador1['hp_maximo'], datos_jugador1['energia_maxima'])
-        elif tipo == 'Fuego':
-            pokemon_jugador1 = PokemonFuego(datos_jugador1['nombre'], datos_jugador1['hp_maximo'], datos_jugador1['energia_maxima'])
-        elif tipo == 'Planta':
-            pokemon_jugador1 = PokemonPlanta(datos_jugador1['nombre'], datos_jugador1['hp_maximo'], datos_jugador1['energia_maxima'])
-        elif tipo == 'Electrico':
-            pokemon_jugador1 = PokemonElectrico(datos_jugador1['nombre'], datos_jugador1['hp_maximo'], datos_jugador1['energia_maxima'])
-            
-        
-        
         datos_jugador2 = CATALOGO_POKEMON[eleccion_jugador_2]
         
-        if tipo == 'Agua':
-            pokemon_jugador2 = PokemonAgua(datos_jugador2['nombre'], datos_jugador2['hp_maximo'], datos_jugador2['energia_maxima'])
-        elif tipo == 'Fuego':
-            pokemon_jugador2 = PokemonFuego(datos_jugador2['nombre'], datos_jugador2['hp_maximo'], datos_jugador2['energia_maxima'])
-        elif tipo == 'Planta':
-            pokemon_jugador2 = PokemonPlanta(datos_jugador2['nombre'], datos_jugador2['hp_maximo'], datos_jugador2['energia_maxima'])
-        elif tipo == 'Electrico':
-            pokemon_jugador2 = PokemonElectrico(datos_jugador2['nombre'], datos_jugador2['hp_maximo'], datos_jugador2['energia_maxima'])
+        tipo_jugador1 = datos_jugador1['tipo']
+        tipo_jugador2 = datos_jugador2['tipo']
         
+        
+        pokemon_jugador1 = crear_pokemon(datos_jugador1)
+        pokemon_jugador2 = crear_pokemon(datos_jugador2)
+       
+        print(f'¡Has seleccionado a {eleccion_jugador_1}')
+        print(f'¡Has seleccionado a {eleccion_jugador_2}')
+        
+        print('¡COMIENZA LA BATALLA!')
+        print(f'{eleccion_jugador_1} ({tipo_jugador1}) vs {eleccion_jugador_2} ({tipo_jugador2})')
+        
+      
+
         
     elif opcion =='2':
         mostrar_catalogo_disponible()
         
         eleccion_jugador_1 = input('Jugador 1, elija el numero de su Pokémon: ')
         print(f'¡Has seleccionado a {eleccion_jugador_1}')
+        
         import random
         eleccion_jugador_2 = random.choice(list(CATALOGO_POKEMON.keys()))
         
-        print(f'¡COMPUTADORA has seleccionado a {eleccion_jugador_2}')
-        print('¡COMIENZA LA BATALLA!')
-        print(f'{eleccion_jugador_1} ({tipo}) vs {eleccion_jugador_2} ({tipo})')
-        
         datos_jugador1 = CATALOGO_POKEMON[eleccion_jugador_1]
+        datos_jugador2 = CATALOGO_POKEMON[eleccion_jugador_2]
+        
+        tipo_jugador1 = datos_jugador1['tipo']
+        tipo_jugador2 = datos_jugador2['tipo']
+        
+        pokemon_jugador1 = crear_pokemon(datos_jugador1)
+        pokemon_jugador2 = crear_pokemon(datos_jugador2)
         
        
         
+        print('Computadora eligiendo combatiente...')
+        print(f'¡La computadora ha seleccionado a {eleccion_jugador_2}')
         
-        if tipo == 'Agua':
-            pokemon_jugador1 = PokemonAgua(datos_jugador1['nombre'], datos_jugador1['hp_maximo'], datos_jugador1['energia_maxima'])
-        elif tipo == 'Fuego':
-            pokemon_jugador1 = PokemonFuego(datos_jugador1['nombre'], datos_jugador1['hp_maximo'], datos_jugador1['energia_maxima'])
-        elif tipo == 'Planta':
-            pokemon_jugador1 = PokemonPlanta(datos_jugador1['nombre'], datos_jugador1['hp_maximo'], datos_jugador1['energia_maxima'])
-        elif tipo == 'Electrico':
-            pokemon_jugador1 = PokemonElectrico(datos_jugador1['nombre'], datos_jugador1['hp_maximo'], datos_jugador1['energia_maxima'])
-            
+        print('¡COMIENZA LA BATALLA!')
+        print(f'{eleccion_jugador_1} ({tipo_jugador1}) vs {eleccion_jugador_2} ({tipo_jugador2})')
         
         
-        datos_jugador2 = CATALOGO_POKEMON[eleccion_jugador_2]
         
-        if tipo == 'Agua':
-            pokemon_jugador2 = PokemonAgua(datos_jugador2['nombre'], datos_jugador2['hp_maximo'], datos_jugador2['energia_maxima'])
-        elif tipo == 'Fuego':
-            pokemon_jugador2 = PokemonFuego(datos_jugador2['nombre'], datos_jugador2['hp_maximo'], datos_jugador2['energia_maxima'])
-        elif tipo == 'Planta':
-            pokemon_jugador2 = PokemonPlanta(datos_jugador2['nombre'], datos_jugador2['hp_maximo'], datos_jugador2['energia_maxima'])
-        elif tipo == 'Electrico':
-            pokemon_jugador2 = PokemonElectrico(datos_jugador2['nombre'], datos_jugador2['hp_maximo'], datos_jugador2['energia_maxima'])
+   
+        
+        
+        
+        
+        
+    
+        
+    
         
         
         
@@ -99,12 +102,7 @@ def menu_batalla():
         
         
         
-        
-        
-        
-        
-        
-        tipo = datos_jugador1['tipo']
+        tipo = datos_jugador['tipo']
         tipo = datos_jugador2['tipo']  
         
         
